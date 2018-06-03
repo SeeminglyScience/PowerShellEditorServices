8898fe14507c34c5b3e060d5a4b5eff0fc9b3d89
Patrick Meinecke <seeminglyscience@gmail.com>
2018-06-03 10:37:46 -0400

    Pass feature flags to Start-EditorServicesHost

--- module/PowerShellEditorServices/Start-EditorServices.ps1
+++ module/PowerShellEditorServices/Start-EditorServices.ps1
@@ -312,7 +312,8 @@ try {
             -BundledModulesPath $BundledModulesPath `
             -EnableConsoleRepl:$EnableConsoleRepl.IsPresent `
             -DebugServiceOnly:$DebugServiceOnly.IsPresent `
-            -WaitForDebugger:$WaitForDebugger.IsPresent
+            -WaitForDebugger:$WaitForDebugger.IsPresent `
+            -FeatureFlags:$FeatureFlags
 
     # TODO: Verify that the service is started
     Log "Start-EditorServicesHost returned $editorServicesHost"
